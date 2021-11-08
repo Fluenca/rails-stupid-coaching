10405f21f6025a4be7405b36b4aeec855a435efb
Philipp Krink <krink.philipp@gmail.com>
2021-11-08 14:25:08 +0100

    Testing done.

---
+++ test/system/questions_test.rb
@@ -0,0 +1,25 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "saying I am going to work makes the coach happy" do
+    visit ask_url
+    fill_in "question", with: "I AM GOING TO WORK"
+    click_on "Ask"
+
+    assert_text "Great!"
+    take_screenshot
+  end
+end
